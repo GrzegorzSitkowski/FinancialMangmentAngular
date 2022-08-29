4dfd25b06c6ec4d301cb6f3e2ce7f7b73aafda72
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2022-08-29 18:10:13 +0200

    Filled Products service

--- src/app/services/products/products.service.ts
+++ src/app/services/products/products.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Product } from 'src/app/models/products/product.model';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService {
+  baseApiUrl: string = environment.baseApiUrl;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getProducts(): Observable<Product[]>{
+    return this.http.get<Product[]>(this.baseApiUrl + '/api/produts');
+  }
 }
