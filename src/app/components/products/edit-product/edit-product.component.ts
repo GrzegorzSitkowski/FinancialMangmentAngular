4dadc5c89bdd336ca14a0e5bf1917d469fa531ae
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2022-08-29 19:31:00 +0200

    Filled edit-Product.Component.ts

--- src/app/components/products/edit-product/edit-product.component.ts
+++ src/app/components/products/edit-product/edit-product.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ProductEdit } from 'src/app/models/products/productEdit.model';
+import { ProductsService } from 'src/app/services/products/products.service';
 
 @Component({
   selector: 'app-edit-product',
@@ -7,9 +10,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditProductComponent implements OnInit {
 
-  constructor() { }
+  productDetail: ProductEdit = {
+    id: '',
+    product: '',
+    price: 0,
+    done: false
+  };
+
+  constructor(private route:ActivatedRoute, private productService: ProductsService, private router:Router) { }
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe({
+      next: (params) => {
+        const id = params.get('id');
+        if(id){
+          this.productService.getProduct(id)
+          .subscribe({
+            next: (response) => {
+              this.productDetail = response;
+            }
+          });
+        }
+      }
+    })
+  }
+
+  updateProduct(){
+    this.productService.updateProduct(this.productDetail.id, this.productDetail)
+    .subscribe({
+      next: (response) => {
+        this.router.navigate(['products']);
+      }
+    });
   }
 
 }
