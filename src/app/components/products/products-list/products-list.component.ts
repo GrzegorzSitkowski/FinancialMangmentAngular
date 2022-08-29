40b4600f4dfd453f27508ff137ec3ba992eca1d4
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2022-08-29 18:14:15 +0200

    Filled Products-list.Component.ts

--- src/app/components/products/products-list/products-list.component.ts
+++ src/app/components/products/products-list/products-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/models/products/product.model';
+import { ProductsService } from 'src/app/services/products/products.service';
 
 @Component({
   selector: 'app-products-list',
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductsListComponent implements OnInit {
 
-  constructor() { }
+  products: Product[] = [];
+  constructor(private productService: ProductsService) { }
 
   ngOnInit(): void {
+    this.productService.getProducts()
+    .subscribe({
+      next:(productApi) => {
+        this.products = productApi as Product[];
+        console.log(this.products);
+      },
+      error: (response) => {
+        console.log(response);
+      }
+    })
   }
 
 }
