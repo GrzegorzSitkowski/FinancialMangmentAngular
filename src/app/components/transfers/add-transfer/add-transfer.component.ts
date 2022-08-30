352253ee8a555f53acc851f0447431e22fcb8c36
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2022-08-30 11:35:07 +0200

    Filled add-transfers.component.ts

--- src/app/components/transfers/add-transfer/add-transfer.component.ts
+++ src/app/components/transfers/add-transfer/add-transfer.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { TransferAdd } from 'src/app/models/transfers/transferAdd.model';
+import { TransfersService } from 'src/app/services/transfers/transfers.service';
 
 @Component({
   selector: 'app-add-transfer',
@@ -6,10 +9,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-transfer.component.css']
 })
 export class AddTransferComponent implements OnInit {
-
-  constructor() { }
+  addTransferRequest: TransferAdd = {
+    name: '',
+    amount: 0,
+    typeId: 0,
+    categoryId: 0,
+    date: new Date(2022,1,1),
+    description: '',
+    accountId: 0
+  }
+  constructor(private transferService: TransfersService, private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  addTransfer(){
+    this.transferService.addTransfer(this.addTransferRequest)
+    .subscribe({
+      next: (transfer) => {
+        this.router.navigate(['transfers']);
+      }
+    });
   }
 
 }
