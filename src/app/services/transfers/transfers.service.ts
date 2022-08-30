e07a11f115eef1253d893d530d936ba55bcc3d61
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2022-08-30 11:09:41 +0200

    Added GetTransfers in TransfersService

--- src/app/services/transfers/transfers.service.ts
+++ src/app/services/transfers/transfers.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Transfer } from 'src/app/models/transfers/transfer.model';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TransfersService {
+  baseApiUrl : string = environment.baseApiUrl;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getTransfers(): Observable<Transfer[]>{
+    return this.http.get<Transfer[]>(this.baseApiUrl + '/api/transfers');
+  }
 }
