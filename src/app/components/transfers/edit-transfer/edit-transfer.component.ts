92097d349c15381f9ec96595df04310ca5dc33e4
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2022-08-30 12:21:34 +0200

    Filled edit-transfer.component.ts

--- src/app/components/transfers/edit-transfer/edit-transfer.component.ts
+++ src/app/components/transfers/edit-transfer/edit-transfer.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { TransferEdit } from 'src/app/models/transfers/transferEdit.model';
+import { TransfersService } from 'src/app/services/transfers/transfers.service';
 
 @Component({
   selector: 'app-edit-transfer',
@@ -7,9 +10,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditTransferComponent implements OnInit {
 
-  constructor() { }
+  transferDetails: TransferEdit = {
+    id: '',
+    name: '',
+    typeId: 0,
+    categoryId: 0,
+    date: new Date(2022,1,1),
+    description: ''
+  };
+  constructor(private route:ActivatedRoute, private transferService: TransfersService, private router: Router) { }
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe({
+      next: (params) => {
+        const id = params.get('id');
+        if(id){
+          this.transferService.getTransfer(id)
+          .subscribe({
+            next: (response) => {
+              this.transferDetails = response;
+            }
+          });
+        }
+      }
+    })
+  }
+
+  updateTransfer(){
+    this.transferService.updateTransfer(this.transferDetails.id, this.transferDetails)
+    .subscribe({
+      next: (response) => {
+        this.router.navigate(['transfers']);
+      }
+    });
   }
 
 }
