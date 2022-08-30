48d85fd63e68da103ff7458d00aca6d6f61209f1
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2022-08-30 11:29:54 +0200

    Added AddTransfer to TransferService

--- src/app/services/transfers/transfers.service.ts
+++ src/app/services/transfers/transfers.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Transfer } from 'src/app/models/transfers/transfer.model';
+import { TransferAdd } from 'src/app/models/transfers/transferAdd.model';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
@@ -14,5 +15,9 @@ export class TransfersService {
 
   getTransfers(): Observable<Transfer[]>{
     return this.http.get<Transfer[]>(this.baseApiUrl + '/api/transfers');
+  }
+
+  addTransfer(addTransferRequest: TransferAdd): Observable<TransferAdd>{
+    return this.http.post<TransferAdd>(this.baseApiUrl + '/api/transfers', addTransferRequest);
   }
 }
